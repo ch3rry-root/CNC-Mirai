2dfbe5d154b4a6b9d4efb03f70a0cb616e4a7f03
cherry.nvme <108516418+ch3rry-root@users.noreply.github.com>
2026-03-28 22:26:50 -0500

    idk

---
+++ tools/xor.py
@@ -0,0 +1,26 @@
+def obfuscate(s, key):
+    k1 = key & 0xff
+    k2 = (key >> 8) & 0xff
+    k3 = (key >> 16) & 0xff
+    k4 = (key >> 24) & 0xff
+    res = []
+    for ch in s:
+        b = ord(ch)
+        b ^= k1
+        b ^= k2
+        b ^= k3
+        b ^= k4
+        res.append(f"\\x{b:02X}")
+    return ''.join(res)
+
+
+
+domain = "example.com\x00"  # Reemplaza con tu dominio
+print("Obfuscated domain:")
+print(obfuscate(domain, 0xdeadbeef))
+
+
+ip = "185.242.3.160\x00"  # Reemplaza con tu IP
+print("Obfuscated IP:")
+print(obfuscate(ip, 0xdeadbeef))
+
